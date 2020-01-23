3d66eeb4ed48e1732285185a7d42b3fc0a126dc2
jjp649171 <59873396+jjp649171@users.noreply.github.com>
2020-01-23 10:41:58 -0300

    4

--- fase02/ex04/minusculo.js
+++ fase02/ex04/minusculo.js
@@ -1,6 +1,8 @@
 function minusculo(palavra) {
 const palavras = palavra;
-return palavra.substring(0,1).concat(palavra.substring(1).toUpperCase())
+return palavra.substring(1,0).toUpperCase()
 
 }
+
+
 
