61b6d1e6c0c58587de44ed2f7cfe211e42e1e4ee
jjp649171 <59873396+jjp649171@users.noreply.github.com>
2020-02-04 09:06:41 -0300

    3

---
+++ fase03/ex03/vogalOuConsoante.js
@@ -0,0 +1,17 @@
+function vogalOuConsoante(e){
+    
+
+
+var e1 = e;
+
+    if (e=='a' || e=='e' || e=='i' || e=='o' || e=='u'){
+
+        return "Vogal";
+        
+        
+    } else {
+        return "Consoante" ;
+    }
+
+
+}
